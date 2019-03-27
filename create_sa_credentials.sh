21a48cb67fc1bcd2001afb8ed27fdfcc25e1d0ab
rbjornstad <roger.bjornstad@nav.no>
2019-03-27 12:41:26 +0100

    New echo... echo... echo...

--- create_sa_credentials.sh
+++ create_sa_credentials.sh
@@ -11,4 +11,5 @@ CLUSTER_NAME=$(echo ${PROJECT_ID} | awk -F- '{print $2"-gcp"}')
 gcloud iam service-accounts keys create tmp.json --iam-account=nais-ci@${PROJECT_ID}.iam.gserviceaccount.com
 openssl enc -e -aes-256-cfb -a -A -k ${KEY} < tmp.json > ${CLUSTER_NAME}-sa-credentials.json.enc
 rm tmp.json
-echo "Created encrypted file ${CLUSTER_NAME}-sa-credentials.json.enc and removed tmp.json"
+echo "Created encrypted file ${CLUSTER_NAME}-sa-credentials.json.enc and removed tmp.json."
+echo "Please make sure to commit encrypted key-file to github"
